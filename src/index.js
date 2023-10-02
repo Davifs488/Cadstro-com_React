ad7940f463ce3e9dbdd3dce3e78c37f5515adecf
Davi Ferreira da Silva <sildv45@gmail.com>
2023-10-01 21:25:34 -0300

    Projeto de cadastro com React

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//Importar módulo
+import ReactDOM from "react-dom";
+
+//
+//Importar o CSS
+import "./estilos.css";
+//
+//Importar o componente Principal
+import Principal from "./Principal";
+
+//
+//Render
+ReactDOM.render(<Principal />, document.getElementById("root"));
